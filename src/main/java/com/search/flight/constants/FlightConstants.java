b25ee507a5791ada95fd591556b0934f9b067287
vaibh <vaibhavdeore5@gmail.com>
2018-08-22 08:44:07 +0530

    Added constant file
    Change-Id: I843bfc1ace4c1a9d1b8882c88f36d5650c639235

---
+++ src/main/java/com/search/flight/constants/FlightConstants.java
@@ -0,0 +1,13 @@
+package com.search.flight.constants;
+
+public class FlightConstants {
+	
+	public static final char PIPE_DELIMETER = '|';
+	public static final char COMMA_DELIMETER = ',';
+	public static final String PROVIDER1 = "provider1.csv";
+	public static final String PROVIDER2 = "provider2.csv";
+	public static final String PROVIDER3 = "provider3.csv";
+	
+	
+
+}
